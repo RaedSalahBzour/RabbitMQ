c6e61baf256dd911e15365ab1c8b6502e71ea8af
Raed Salah Bzour <raedbzour77@gmail.com>
2025-09-14 16:02:16 +0300

    adding topic exchange

---
+++ Topic.Consumer/Program.cs
@@ -0,0 +1,24 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    //Here , do some processing .
+
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue: "q.health3",
+                     autoAck: true,
+                     consumer: consumer);
---
+++ Topic.Producer/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+const string message = "Topic from dot net";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+    exchange: "amq.topic",
+    routingKey: "h.y",
+    basicProperties: null,
+    body: body
+);
+
+Console.WriteLine($" [x] Sent {message}");
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
