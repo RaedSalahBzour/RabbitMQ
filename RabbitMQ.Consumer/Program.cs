f9f2ca8954d128dcc0a207384af3b30296137313
Raed Salah Bzour <raedbzour77@gmail.com>
2025-09-14 13:52:54 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(
+    queue: "q.direct",
+    autoAck: true,
+    consumer: consumer
+);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
