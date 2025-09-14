0fd8a28f4a19d1304e58e689f12a4e4967a08717
Raed Salah Bzour <raedbzour77@gmail.com>
2025-09-14 18:56:51 +0300

    adding header exchange

---
+++ Header.Consumer/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    //Here , do some processing .
+
+    Console.WriteLine($" [x] Received {message}");
+};
+
+channel.BasicConsume(queue: "q.header1",
+                     autoAck: true,
+                     consumer: consumer);
+channel.BasicConsume(queue: "q.header2",
+                     autoAck: true,
+                     consumer: consumer);
---
+++ Header.Producer/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+var properties = channel.CreateBasicProperties();
+properties.Persistent = false;
+
+Dictionary<string, object> dictionary = new Dictionary<string, object>();
+dictionary.Add("name", "info");
+dictionary.Add("secondName", "error");
+
+properties.Headers = dictionary;
+const string message = "message from header asp dot net application";
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "amq.headers",
+                     routingKey: string.Empty,
+                     basicProperties: properties,
+                     body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
