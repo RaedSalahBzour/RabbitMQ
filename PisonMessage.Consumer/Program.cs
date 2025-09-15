141f5f02db5f4fabd7223f08fb1a4b4d704d42d6
Raed Salah Bzour <raedbzour77@gmail.com>
2025-09-15 16:29:53 +0300

    adding and handlingthe poison message

---
+++ PisonMessage.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+Console.WriteLine("[*] Waiting for messages.");
+
+var consumer = new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    // Here, do some processing.
+
+    // reject and requeue
+    channel.BasicNack(ea.DeliveryTag, false, true);
+
+    Console.WriteLine($"[x] Received {message}");
+};
+
+channel.BasicConsume(queue: "queueName",
+                     autoAck: false,
+                     consumer: consumer);
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
